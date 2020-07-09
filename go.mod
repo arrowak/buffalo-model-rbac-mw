2c69f61b68636cc3349c5c763f759c2751d028e6
arun.kolhapur <arun.kolhapur@verse.in>
2020-07-09 10:08:12 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module buffalo-model-rbac-mw
+
+go 1.14
+
+require (
+	github.com/casbin/casbin/v2 v2.8.2
+	github.com/gobuffalo/buffalo v0.16.10
+	github.com/pkg/errors v0.9.1
+)
---
+++ modelRbac.go
@@ -0,0 +1,34 @@
+package modelRbac
+
+import (
+	"net/http"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/gobuffalo/buffalo"
+	"github.com/pkg/errors"
+)
+
+// RoleGetter must return the role of the user who made the request
+type RoleGetter func(buffalo.Context) (string, error)
+
+// New enables cashbin rbac
+func New(e *casbin.Enforcer, r RoleGetter) buffalo.MiddlewareFunc {
+	return func(next buffalo.Handler) buffalo.Handler {
+		return func(c buffalo.Context) error {
+			role, err := r(c)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+
+			res, err := e.Enforce(role, c.Request().URL.Path, c.Request().Method)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+			if res {
+				return next(c)
+			}
+
+			return c.Error(http.StatusUnauthorized, errors.New("You are unauthorized to perform the requested action"))
+		}
+	}
+}
